6dc32a78e0279200e6a683884d3ebac41d69b198
Emilie <emilie.chnvt@gmail.com>
2024-11-30 15:57:37 +0100

    function createBoard to create each square of the grid

---
+++ js/main.js
@@ -0,0 +1,21 @@
+//DOMContentLoaded when the HTML document has been completely parsed.
+document.addEventListener('DOMContentLoaded', ()=>{
+    const grid = document.querySelector(".grid");
+    const width = 8;
+    const squares = []
+
+
+
+
+
+
+    function createBoard(){
+        for(let i = 0; i < width*width ; i++){
+            const square = document.createElement("div");
+            grid.appendChild(square);
+            squares.push(squares)
+        }
+
+    }
+    createBoard()
+})
